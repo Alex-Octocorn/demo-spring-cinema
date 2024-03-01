1a1e5eb839fcc5c0fa3a5d687b145121f72b272f
Alexandre Devos <a.devos@octocorn.fr>
2024-03-01 13:33:09 +0100

    wip: hard coded security :sparkles: :poop:

---
+++ src/main/java/fr/octorn/cinemacda4/security/SecurityConfig.java
@@ -0,0 +1,69 @@
+package fr.octorn.cinemacda4.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        BCryptPasswordEncoder encoder = passwordEncoder();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(encoder.encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails employee = User.builder()
+                .username("employee")
+                .password(encoder.encode("employee"))
+                .roles("USER", "EMPLOYEE")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER", "EMPLOYEE", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, employee, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityChain(
+            HttpSecurity http
+    ) throws Exception {
+            http.authorizeHttpRequests(
+                    authorizedRequests -> authorizedRequests
+
+                            .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/seances/{id}/reserver").hasRole("USER")
+
+            ).formLogin(Customizer.withDefaults());
+
+            http.httpBasic(Customizer.withDefaults());
+
+            http.csrf(AbstractHttpConfigurer::disable);
+
+            return http.build();
+    }
+}
